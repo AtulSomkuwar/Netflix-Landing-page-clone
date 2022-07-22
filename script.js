a804257cf6b6f88b0ecfccf798ebf1aa8f0b43f0
PujaNeralwar <102819825+PujaNeralwar@users.noreply.github.com>
2022-07-23 00:41:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+const que1=document.getElementById('ans1');
+const btn1=document.getElementById('que-1');
+btn1.onclick=function(){
+    if(que1.style.display !=="none"){
+        que1.style.display="none";
+    }
+    else{
+        que1.style.display="block";
+    }
+}
+const que2=document.getElementById('ans2');
+const btn2=document.getElementById('que-2');
+btn2.onclick=function(){
+    if(que2.style.display !=="none"){
+        que2.style.display="none";
+    }
+    else{
+        que2.style.display="block";
+    }
+}
+const que3=document.getElementById('ans3');
+const btn3=document.getElementById('que-3');
+btn3.onclick=function(){
+    if(que3.style.display !=="none"){
+        que3.style.display="none";
+    }
+    else{
+        que3.style.display="block";
+    }
+}
+const que4=document.getElementById('ans4');
+const btn4=document.getElementById('que-4');
+btn4.onclick=function(){
+    if(que4.style.display !=="none"){
+        que4.style.display="none";
+    }
+    else{
+        que4.style.display="block";
+    }
+}
+const que5=document.getElementById('ans5');
+const btn5=document.getElementById('que-5');
+btn5.onclick=function(){
+    if(que5.style.display !=="none"){
+        que5.style.display="none";
+    }
+    else{
+        que5.style.display="block";
+    }
+}
+const que6=document.getElementById('ans6');
+const btn6=document.getElementById('que-6');
+btn6.onclick=function(){
+    if(que6.style.display !=="none"){
+        que6.style.display="none";
+    }
+    else{
+        que6.style.display="block";
+    }
+}
